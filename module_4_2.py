416c2d07e9faada9f2a1ff84eef9d7b9a52748f9
Emma777always <mariel_k23@yahoo.com>
2024-12-04 14:03:55 +0000

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -2,5 +2,5 @@ def test_function():
     def inner_function():
         print('Я в области видимости функции test_function')
     inner_function()
-
+#inner_function() vne funkcii test_function vydaet oshibku
 test_function()
